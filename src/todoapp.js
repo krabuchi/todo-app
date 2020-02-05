70e8c5b1b42fa942d65dd88c819d85cfee3357bb
krabuchi <shashu95@yahoo.com>
2020-02-05 16:45:44 +0530

    remove item function added bg color change

--- src/todoapp.js
+++ src/todoapp.js
@@ -2,77 +2,74 @@ import React from 'react';
 import ItemsList from './itemslist';
 
 class TodoApp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        items: [],
-        text: '',
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.change =  this.change.bind(this);
-      this.handleRemove = this.handleRemove.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      text: '',
     }
-
-    componentDidMount() {
-      this.setState({items:JSON.parse(localStorage.getItem('items'))|| []});
-    }
-    
-    handleChange(e){
-      this.setState({text: e.target.value})
-    }
-    
-    handleSubmit(e) {
-      e.preventDefault();
-      if(!this.state.text.length) return;
-      
-      this.setState({
-          items: this.state.items.concat({
-            id: Date.now(),
-            text: this.state.text,
-            checked: false
-          }),
-          text: ''
-        }
-      );
-      console.log(this.state.items)
-      localStorage.setItem('items', JSON.stringify(this.state.items));
-    }
-    
-    change(id) {
-      const upDate = this.state.items.map(item => {
-        if(item.id === id) {
-          item.checked = !item.checked;
-        }
-        return item;
-      })
-      this.setState({items: upDate});
-      localStorage.setItem('items', JSON.stringify(this.state.items));
-    }
-    
-    handleRemove(id) {
-      const remainder = this.state.items.filter(item => {
-        if(item.id !== id) return item;
-      });
-      this.setState({items: remainder});
-    }
-  
-    render() {
-      
-      return (
-        <div className="container">
-          <h3>Todo App</h3>
-          <form onSubmit={this.handleSubmit}>
-            <input type='text' 
-              onChange={this.handleChange} 
-              value={this.state.text} 
-            />
-            <button>+ Add Item</button>
-          </form>
-          <ItemsList items={this.state.items} handleChange={this.change} handleRemove={this.handleRemove}/>
-        </div>
-      )
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.change =  this.change.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
   }
 
-  export default TodoApp;
+  componentDidMount() {
+    this.setState({items:JSON.parse(localStorage.getItem('items'))|| []});
+  }
+  
+  handleChange(e){
+    this.setState({text: e.target.value})
+  }
+    
+  handleSubmit(e) {
+    e.preventDefault();
+    if(!this.state.text.length) return;
+    const tem = this.state.items.concat({
+      id: Date.now(),
+      text: this.state.text,
+      checked: false
+    });
+    
+    this.setState({items: tem, text: ''});
+    console.log(this.state.items)
+    localStorage.setItem('items', JSON.stringify(tem));
+  }
+    
+  change(id) {
+    const upDate = this.state.items.map(item => {
+      if(item.id === id) {
+        item.checked = !item.checked;
+      }
+      return item;
+    })
+    this.setState({items: upDate});
+    localStorage.setItem('items', JSON.stringify(upDate));
+  }
+    
+  handleRemove(id) {
+    const remainder = this.state.items.filter(item => {
+      if(item.id !== id) return item;
+    });
+    this.setState({items: remainder});
+    localStorage.setItem('items', JSON.stringify(remainder));
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <h3>Todo App</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' 
+            onChange={this.handleChange} 
+            value={this.state.text} 
+          />
+          <button>+ Add Item</button>
+        </form>
+        <ItemsList items={this.state.items} handleChange={this.change} handleRemove={this.handleRemove}/>
+      </div>
+    )
+  }
+}
+
+export default TodoApp;
